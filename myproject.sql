0d1096b6337ff6e82198cc3fbeed8aacd4333485
ShubhamRwt <shubhamrwt02@gmail.com>
2018-10-10 01:03:40 +0530

    First commit

---
+++ myproject.sql
@@ -0,0 +1,20 @@
+create database myproject;
+use myproject;
+
+create table student(
+roll int primary key,
+name varchar(100),
+PhoneNo char(50),
+Address varchar(100),
+Stream varchar(100),
+Percentage int,
+email char(40),
+gender char(20),
+DOJ varchar(20),
+fname varchar(90),
+mname varchar(90),
+fee varchar(30)
+);
+
+
+
